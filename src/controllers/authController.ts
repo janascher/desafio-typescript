1f63d3f28ad8a3811ec5b04482be45c639e589a4
rogeriokotsubo <rogeriokotsubo@gmail.com>
2022-11-16 21:21:20 +0000

    feat: login route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from 'express';
 
 import { AuthServices } from '../services/authenticate';
+import { LoginData } from '../models/interfaces';
+import jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export class AuthController
 {
@@ -13,7 +17,21 @@ export class AuthController
 
     public async login(req: Request, res: Response)
     {
-        const result = await this.authServices.login();
+        const loginData : LoginData = req.body;
+        const result = await this.authServices.login(loginData);
+
+        if (result.error === null) {
+            const { userID, userType, userEmail, userName } = result;
+            const alreadyHasToken = req.cookies.token;
+            if (alreadyHasToken) { res.clearCookie('token'); };
+            if (process.env.JWT_SECRET){
+                const token = jwt.sign({ userID, userType, userEmail, userName}, process.env.JWT_SECRET, { expiresIn: 7200 });
+                res.cookie('token', token, { httpOnly: true });
+                res.status(200).json({ userType });
+            }     
+        } else {
+            res.status(result.status).json({message: result.error});
+        };        
     }
 
     public logout(req: Request, res: Response)
